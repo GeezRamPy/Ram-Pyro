3624cabf2bfb27cd45a58d9967bbf911817073e7
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2023-01-19 23:49:57 +0700

    Update start.py

--- rams/modules/bot/start.py
+++ rams/modules/bot/start.py
@@ -14,4 +14,4 @@ from pyrogram import filters
 
 @app.on_message(filters.command("start") & filters.private)
 async def start(client, message):
-   await message.reply_text("Hey Geez Userbot Assistant here")
+   await message.reply_text("Hey RamPyro-Bot Assistant here")
