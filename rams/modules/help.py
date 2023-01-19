d8939955812a3e05ac77a4a5e6782a26fb58676f
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2023-01-20 04:05:16 +0700

    baa

--- rams/modules/help.py
+++ rams/modules/help.py
@@ -52,11 +52,11 @@ async def module_help(client: Client, message: Message):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @GeezSupport >< @ramsupportt •",
+                f"```{str(ac)}```\n• @GeezRam >< @UserbotCh •",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
-                f"**Usage:** `.help broadcast` **To View Module Information**"
+                f"**Usage:** `{cmd}help broadcast` **To View Module Information**"
             )
             return
 
@@ -87,15 +87,15 @@ async def module_helper(client: Client, message: Message):
     elif not message.reply_to_message and len(cmd) == 1:
         ac = PrettyTable()
         ac.header = False
-        ac.title = "Geez Pyro Plugins"
+        ac.title = "Ram Pyro Plugins"
         ac.align = "l"
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @GeezSupport >< @ramsupportt •"
+            message, f"```{str(ac)}```\n• @GeezRam >< @UserbotCh •"
         )
         await message.reply(
-            f"**Usage**:`.help broadcast` **To View Module details**"
+            f"**Usage**:`{cmd}help broadcast` **To View Module details**"
         )
 
     if help_arg:
@@ -104,7 +104,7 @@ async def module_helper(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @GeezSupport >< @ramsupportt"
+            this_command += "© @GeezRam >< @UserbotCh"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
@@ -126,3 +126,4 @@ def add_command_help(module_name, commands):
                 command_dict[x[0]] = x[1]
 
     CMD_HELP[module_name] = command_dict
+#null
