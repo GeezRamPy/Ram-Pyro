c449b0623466482d5c19c32338ab812f7824d93a
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2023-01-21 00:09:01 +0700

    Update help.py

--- rams/modules/help.py
+++ rams/modules/help.py
@@ -103,7 +103,7 @@ async def module_helper(client: Client, message: Message):
             commands: dict = CMD_HELP[help_arg]
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
-                this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
+                this_command += f"  •  **Command:** `{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
             this_command += "© @GeezRam >< @UserbotCh"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
