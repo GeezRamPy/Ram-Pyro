e478afc7e0f0d50ecf75c2aeefe9ed0190b37718
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2023-01-24 02:09:55 +0700

    Update ping.py

--- rams/modules/ping.py
+++ rams/modules/ping.py
@@ -139,8 +139,7 @@ async def module_ping(client: Client, message: Message):
     elif not message.reply_to_message and len(cdm) == 1:
         try:
             nice = await client.get_inline_bot_results(bot=bot_username, query="rama")
-            await asyncio.gather(
-                message.reply_text(message),
+            await message.reply_text(
                 client.send_inline_bot_result(
                     message.chat.id, nice.query_id, nice.results[0].id
                 ),
