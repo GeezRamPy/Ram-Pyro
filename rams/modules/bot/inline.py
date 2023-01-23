6b7108a72b92c2b2511bc74dbda73447abc0cece
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2023-01-24 00:39:34 +0700

    Update inline.py

--- rams/modules/bot/inline.py
+++ rams/modules/bot/inline.py
@@ -104,7 +104,7 @@ async def ping_function(message: Message, answers):
                 msg, parse_mode=ParseMode.HTML, disable_web_page_preview=True
             ),
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton(text="•open inline•", calback_data="reopen")]]
+                [[InlineKeyboardButton(text="•open inline•", callback_data="reopen")]]
             ),
         )
     )
