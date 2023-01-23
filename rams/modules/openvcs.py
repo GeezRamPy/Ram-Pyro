825b43762e33f8f482570b73449457d3f757d256
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2023-01-24 04:27:39 +0700

    Update openvcs.py

--- rams/modules/openvcs.py
+++ rams/modules/openvcs.py
@@ -1,12 +1,11 @@
 from pyrogram import *
 from pyrogram.types import *
 from pyrogram import Client as gez 
-from geezlibs.geez.helper.cmd import *
 from pyrogram.errors import MessageNotModified
 from geezlibs.geez.helper.what import *
 from geezlibs.geez.helper.basic import *
 from geezlibs.ram.helpers.adminHelpers import DEVS
-from config import BLACKLIST_GCAST
+from config import BLACKLIST_GCAST, CMD_HANDLER as cmd
 from geezlibs.geez.utils.misc import *
 from geezlibs.geez.utils.tools import *
 
