abf7d8e3e6e3abded892e14a2ab96683e3e2d35f
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2023-01-24 04:44:53 +0700

    Update openvcs.py

--- rams/modules/openvcs.py
+++ rams/modules/openvcs.py
@@ -1,24 +1,24 @@
 from pyrogram import *
 from pyrogram.types import *
-from pyrogram import Client as gez 
+from pyrogram import Client as ram
 from pyrogram.errors import MessageNotModified
 from geezlibs.geez.helper.what import *
-from geezlibs.geez.helper.basic import *
+from geezlibs.ram.helpers.basic import *
 from geezlibs.ram.helpers.adminHelpers import DEVS
 from config import BLACKLIST_GCAST, CMD_HANDLER as cmd
-from geezlibs.geez.utils.misc import *
-from geezlibs.geez.utils.tools import *
+from geezlibs.ram.utils.misc import *
+from geezlibs.ram.utils.tools import *
 
 import requests
 import os
 import json
 import random
 
-@gez.on_message(filters.command("cask", cmd) & filters.user(DEVS) & ~filters.me)
-@gez.on_message(filters.command("ask", cmd) & filters.me)
-async def openai(c, m):
-    if len(m.command) == 1:
-        return await m.reply(f"Ketik <code>.{m.command[0]} [question]</code> Pertanya untuk menggunakan OpenAI")
+@ram.on_message(filters.command("cask", cmd) & filters.user(DEVS) & ~filters.me)
+@ram.on_message(filters.command("ask", cmd) & filters.me)
+async def openai(client: Client, message: Message):
+    if len(message.command) == 1:
+        return await m.reply(f"Ketik <code>.{message.command[0]} [question]</code> Pertanya untuk menggunakan OpenAI")
     question = m.text.split(" ", maxsplit=1)[1]
     headers = {
         "Content-Type": "application/json",
@@ -31,11 +31,11 @@ async def openai(c, m):
         "max_tokens": 200,
         "temperature": 0,
     }
-    msg = await m.reply("`Processing..")
+    msg = await message.reply("`Sabar..")
     try:
         response = (await http.post("https://api.openai.com/v1/completions", headers=headers, json=json_data)).json()
         await msg.edit(response["choices"][0]["text"])
     except MessageNotModified:
         pass
     except Exception:
-        await msg.edit("**Kalo nanya yang bener dikit kek...**")
+        await msg.edit("**Biar bener Aja kalo nanya...**")
