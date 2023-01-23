ce10c9fde22b378ff02cb8f946ac90bc4ebec04e
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2023-01-24 01:13:25 +0700

    Update ping.py

--- rams/modules/ping.py
+++ rams/modules/ping.py
@@ -140,7 +140,7 @@ async def module_ping(client: Client, message: Message):
         try:
             nice = await client.get_inline_bot_results(bot=bot_username, query="rama")
             await asyncio.gather(
-                message.delete(),
+                message.reply_text(),
                 client.send_inline_bot_result(
                     message.chat.id, nice.query_id, nice.results[0].id
                 ),
