1b071878c3808309cfa4fa76fcdb100bc0b79908
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2023-01-24 02:37:31 +0700

    Update ping.py

--- rams/modules/ping.py
+++ rams/modules/ping.py
@@ -144,4 +144,3 @@ async def module_ping(client: Client, message: Message):
                 client.send_inline_bot_result(
                     message.chat.id, nice.query_id, nice.results[0].id),
             )
-            
