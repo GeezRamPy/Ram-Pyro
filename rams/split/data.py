d96c0d7bcdc5fe02ade372027a9901d05929941b
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2023-01-24 15:57:04 +0700

    Update data.py

--- rams/split/data.py
+++ rams/split/data.py
@@ -4,7 +4,7 @@ from config import CMD_HANDLER as cmd
 class Data:
 
     text_help_menu = (
-        f"**◇ Menu Inline RamPyro-Master ◇\n╰☞Command Handler Mu adalah : {cmd}**"
+        f"**◇ Menu Inline RamPyro-Master ◇**"
         .replace(",", "")
         .replace("[", "")
         .replace("]", "")
