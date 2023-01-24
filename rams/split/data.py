226a7319962db85933896b9311df53626275e37a
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2023-01-24 15:37:52 +0700

    Update data.py

--- rams/split/data.py
+++ rams/split/data.py
@@ -4,10 +4,10 @@ from config import CMD_HANDLER as cmd
 class Data:
 
     text_help_menu = (
-        f"**Ini List Help Lo Ya Anjing!!!\nJangan Asal Pencet.**"
+        f"**◇ Menu Inline RamPyro-Master ◇\n╰☞Command Handler Mu adalah : {cmd}**"
         .replace(",", "")
         .replace("[", "")
         .replace("]", "")
         .replace("'", "")
     )
-    reopen = [[InlineKeyboardButton("Re-Open", callback_data="reopen")]]
+    reopen = [[InlineKeyboardButton("⇕ open menu ⇕", callback_data="reopen")]]
