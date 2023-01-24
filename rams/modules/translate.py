b4545ceb619bec2f0d1007c858072cba1f29af03
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2023-01-24 17:28:37 +0700

    Update translate.py

--- rams/modules/translate.py
+++ rams/modules/translate.py
@@ -1,73 +1,80 @@
-# Credits: @mrismanaziz
-# Copyright (C) 2022 Pyro-ManUserbot
+# if you can read this, this meant you use code from Geez Ram Project
+# this code is from somewhere else
+# please dont hestitate to steal it
+# because Geez and Ram doesn't care about credit
+# at least we are know as well
+# who Geez and Ram is
 #
-# This file is a part of < https://github.com/mrismanaziz/PyroMan-Userbot/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
 #
-# t.me/SharingUserbot & t.me/Lunatic0de
+# kopas repo dan hapus credit, ga akan jadikan lu seorang developer
+# ©2023 Geez & Ram Team
 
-from gpytranslate import Translator
-from pyrogram import Client, enums, filters
+import os
+from pyrogram import filters, Client
 from pyrogram.types import Message
-from geezlibs.ram.helpers.basic import edit_or_reply
-from config import CMD_HANDLER as cmd
+from py_trans import Async_PyTranslator
+from geezlibs.ram.helpers.utility import get_arg
+from config import CMD_HANDLER as cmds
 
-from .help import add_command_help
-
-
-@Client.on_message(filters.me & filters.command(["tr", "trt", "translate"], cmd))
-async def translate(client: Client, message: Message):
-    trl = Translator()
-    if message.reply_to_message and (
-        message.reply_to_message.text or message.reply_to_message.caption
-    ):
-        input_str = (
-            message.text.split(None, 1)[1]
-            if len(
-                message.command,
-            )
-            != 1
-            else None
-        )
-        target = input_str or "id"
-        if message.reply_to_message.text:
-            text = message.reply_to_message.text
-        else:
-            text = message.reply_to_message.caption
-        try:
-            tekstr = await trl(text, targetlang=target)
-        except ValueError as err:
-            await edit_or_reply(
-                message,
-                f"**ERROR:** `{str(err)}`",
-                parse_mode=enums.ParseMode.MARKDOWN,
-            )
-            return
+@Client.on_message(filters.command(["tr", "translate"], cmds) & filters.me)
+async def pytrans_tr(_, message: Message):
+  tr_msg = await message.edit("`Processing...`")
+  r_msg = message.reply_to_message
+  args = get_arg(message)
+  if r_msg:
+    if r_msg.text:
+      to_tr = r_msg.text
     else:
-        input_str = (
-            message.text.split(None, 2)[1]
-            if len(
-                message.command,
-            )
-            != 1
-            else None
-        )
-        text = message.text.split(None, 2)[2]
-        target = input_str or "id"
-        try:
-            tekstr = await trl(text, targetlang=target)
-        except ValueError as err:
-            await edit_or_reply(
-                message,
-                "**ERROR:** `{}`".format(str(err)),
-                parse_mode=enums.ParseMode.MARKDOWN,
-            )
-            return
-    await edit_or_reply(
-        message,
-        f"**Diterjemahkan ke:** `{target}`\n```{tekstr.text}```\n\n**Bahasa yang Terdeteksi:** `{(await trl.detect(text))}`",
-        parse_mode=enums.ParseMode.MARKDOWN,
-    )
+      return await tr_msg.edit("`Reply to a message that contains text!`")
+    # Checks if dest lang is defined by the user
+    if not args:
+      return await tr_msg.edit(f"`Please define a destination language!`!`")
+    # Setting translation if provided
+    else:
+      sp_args = args.split(" ")
+      if len(sp_args) == 2:
+        dest_lang = sp_args[0]
+        tr_engine = sp_args[1]
+      else:
+        dest_lang = sp_args[0]
+        tr_engine = "google"
+  elif args:
+    # Splitting provided arguments in to a list
+    a_conts = args.split(None, 2)
+    # Checks if translation engine is defined by the user
+    if len(a_conts) == 3:
+      dest_lang = a_conts[0]
+      tr_engine = a_conts[1]
+      to_tr = a_conts[2]
+    else:
+      dest_lang = a_conts[0]
+      to_tr = a_conts[1]
+      tr_engine = "google"
+  # Translate the text
+  py_trans = Async_PyTranslator(provider=tr_engine)
+  translation = await py_trans.translate(to_tr, dest_lang)
+  # Parse the translation message
+  if translation["status"] == "success":
+    tred_txt = f"""
+**Translation Engine**: `{translation["engine"]}`
+**Translated to:** `{translation["dest_lang"]}`
+**Translation:**
+`{translation["translation"]}`
+"""
+    if len(tred_txt) > 4096:
+      await tr_msg.edit("`Wah!! Translated Text So Long Tho!, Give me a minute, I'm sending it as a file!`")
+      tr_txt_file = open("translated.txt", "w+")
+      tr_txt_file.write(tred_txt)
+      tr_txt_file.close()
+      await tr_msg.reply_document("ptranslated_NEXAUB.txt")
+      os.remove("ptranslated.txt")
+      await tr_msg.delete()
+    else:
+      await tr_msg.edit(tred_txt)
 
-
+add_command_help(
+    "translate",
+    [
+        [f"{cmds}tr", "Translate some text by give a text or reply that text/caption."],
+    ],
+)
