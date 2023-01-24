e6125cb5da22c80f98ec7b737106d652bcc93b8c
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2023-01-24 07:53:49 +0700

    Update inline.py

--- rams/split/inline.py
+++ rams/split/inline.py
@@ -42,8 +42,11 @@ def paginate_help(page_number, loaded_modules, prefix):
             ],
             [
                 InlineKeyboardButton(
-                    text="•Tutup Menu•", callback_data=f"close_help")
+                    text="•Tutup Menu•", callback_data=f"close_help"),
             ],
+            [   
+                InlineKeyboardButton(
+                    text="•Tutup Inline•", callback_data=f"close")
         ]]
     return pairs
 
