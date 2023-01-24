50605c04dc6a2a73570dfbffb149c69940ea476e
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2023-01-24 07:09:29 +0700

    Update inline.py

--- rams/split/inline.py
+++ rams/split/inline.py
@@ -38,7 +38,7 @@ def paginate_help(page_number, loaded_modules, prefix):
                 InlineKeyboardButton(
                     text="•⇚•", callback_data=f"{prefix}_prev({modulo_page})"),
                 InlineKeyboardButton(
-                    text="•⇛•", callback_data=f"{prefix}_next({modulo_page})"),
+                    text="•⇛•", callback_data=f"{prefix}_next({modulo_page})")
             ],
             [
                 InlineKeyboardButton(
