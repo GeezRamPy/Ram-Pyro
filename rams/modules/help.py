474b5d1f3b6863ec06f78ad9ffffb202750c1b04
GeezRamPy <123511546+GeezRamPy@users.noreply.github.com>
2023-01-25 16:53:56 +0700

    Update help.py

--- rams/modules/help.py
+++ rams/modules/help.py
@@ -46,7 +46,7 @@ async def module_help(client: Client, message: Message):
             print(f"{e}")
             ac = PrettyTable()
             ac.header = False
-            ac.title = "Ram Pyro Plugins"
+            ac.title = "Ram Pyro Modules"
             ac.align = "l"
             for x in split_list(sorted(CMD_HELP.keys()), 2):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
@@ -63,9 +63,9 @@ async def module_help(client: Client, message: Message):
     if help_arg:
         if help_arg in CMD_HELP:
             commands: dict = CMD_HELP[help_arg]
-            this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
+            this_command = f"《✧ {str(help_arg).upper()}✧》**\n\n"
             for x in commands:
-                this_command += f"  •  **Command:** `{cdm}{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
+                this_command += f"  •  **Perintah:** `{cdm}{str(x)}`\n  •  **Fungsi:** `{str(commands[x])}`\n\n"
             this_command += "© @UserbotCh"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
@@ -87,7 +87,7 @@ async def module_helper(client: Client, message: Message):
     elif not message.reply_to_message and len(cmd) == 1:
         ac = PrettyTable()
         ac.header = False
-        ac.title = "Ram Pyro Plugins"
+        ac.title = "Ram Pyro Modules"
         ac.align = "l"
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
@@ -101,9 +101,9 @@ async def module_helper(client: Client, message: Message):
     if help_arg:
         if help_arg in CMD_HELP:
             commands: dict = CMD_HELP[help_arg]
-            this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
+            this_command = f"《✧**Help For {str(help_arg).upper()}✧》**\n\n"
             for x in commands:
-                this_command += f"  •  **Command:** `{cdm}{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
+                this_command += f"  •  **Perintah:** `{cdm}{str(x)}`\n  •  **Fungsi:** `{str(commands[x])}`\n\n"
             this_command += "© @GeezRam >< @UserbotCh"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
