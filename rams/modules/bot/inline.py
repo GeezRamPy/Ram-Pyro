173d60ec0aa67513d78decf37082728caf6bc154
GeezRamPy <123511546+GeezRamPy@users.noreply.github.com>
2023-01-26 14:54:13 +0700

    Update inline.py

--- rams/modules/bot/inline.py
+++ rams/modules/bot/inline.py
@@ -106,7 +106,7 @@ async def ping_function(message: Message, answers):
                 msg, parse_mode=ParseMode.HTML, disable_web_page_preview=True
             ),
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton(text="❈ sᴜᴘᴘᴏʀᴛ ❈", url="t.me/GeezRam")], [InlineKeyboardButton(text="❈ ᴛᴜᴛᴜᴘ ❈", callback_data="close_help"), InlineKeyboardButton(text="❈ ʙᴜᴋᴀ ❈", callback_data="helper")]]
+                [[InlineKeyboardButton(text="❈ Oᴡɴᴇʀ ❈", url=f"{message.from_user}")]]
             ),
         )
     )
