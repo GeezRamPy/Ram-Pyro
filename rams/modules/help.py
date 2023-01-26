cf0cdc2d5deeebd79f0ae3242ace710ad7c4b455
GeezRamPy <123511546+GeezRamPy@users.noreply.github.com>
2023-01-26 14:45:36 +0700

    Update help.py

--- rams/modules/help.py
+++ rams/modules/help.py
@@ -35,7 +35,7 @@ async def module_help(client: Client, message: Message):
         help_arg = " ".join(cmd[1:])
     elif not message.reply_to_message and len(cmd) == 1:
         try:
-            nice = await client.get_inline_bot_results(bot=bot_username, query="rama")
+            nice = await client.get_inline_bot_results(bot=bot_username, query="helper")
             await asyncio.gather(
                 client.send_inline_bot_result(
                     message.chat.id, nice.query_id, nice.results[0].id),
