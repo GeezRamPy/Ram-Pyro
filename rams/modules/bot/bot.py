6b6e5bb1d38d25d0e3d0486d217a0e4bff0736f7
GeezRamPy <123511546+GeezRamPy@users.noreply.github.com>
2023-01-26 16:57:29 +0700

    Update bot.py

--- rams/modules/bot/bot.py
+++ rams/modules/bot/bot.py
@@ -65,7 +65,7 @@ async def _callbacks(_, callback_query: CallbackQuery):
             callback_query.inline_message_id,
             "**⋙ MENU TELAH DITUTUP ⋘**",
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton(text="⇕ ʙᴜᴋᴀ ʟᴀɢɪ ⇕", callback_data="reopen")], [InlineKeyboardButton(text="⇕ ᴛᴜᴛᴜᴘ ᴀᴊᴀ ⇕", callback_data="close")]]
+                [[InlineKeyboardButton(text="⇕ ʙᴜᴋᴀ ʟᴀɢɪ ⇕", callback_data="reopen")], [InlineKeyboardButton(text="⇕ ᴛᴜᴛᴜᴘ ᴀᴊᴀ ⇕", callback_data="tutup")]]
             ),
         )
         return
