a496319ce312e6e1360f881b7b514aeb918045d7
GeezRamPy <123511546+GeezRamPy@users.noreply.github.com>
2023-01-26 23:30:17 +0700

    Update inline.py

--- rams/modules/bot/inline.py
+++ rams/modules/bot/inline.py
@@ -107,7 +107,7 @@ async def ping_function(message: Message, answers):
                 msg, parse_mode=ParseMode.HTML, disable_web_page_preview=True
             ),
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton(text="❈ Oᴡɴᴇʀ ❈", url=f"tg://user?id={user.id}")]]
+                [[InlineKeyboardButton(text="❈ Oᴡɴᴇʀ ❈", url=f"tg://openmessage?user_id={ID_OWNER}")]]
             ),
         )
     )
