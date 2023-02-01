5eb82fbba4b8a446086b9abe1e7ec413aa112471
GeezRamPy <123511546+GeezRamPy@users.noreply.github.com>
2023-02-01 16:07:39 +0700

    Update inline.py

--- rams/modules/bot/inline.py
+++ rams/modules/bot/inline.py
@@ -79,7 +79,7 @@ async def alive_function(message: Message, answers):
                 msg, parse_mode=ParseMode.HTML, disable_web_page_preview=True
             ),
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton(f"☬ {message.from_user} ☬", url=f"tg://openmessage?user_id={message.from_user.id}")]]
+                [[InlineKeyboardButton(f"☬ {message} ☬", url=f"tg://openmessage?user_id={message.from_user.id}")]]
             ),
         )
     )
