fb694ca436016a82926bd2909652aaecedf4baa0
GeezRamPy <123511546+GeezRamPy@users.noreply.github.com>
2023-02-04 00:13:37 +0700

    Update cplink.py

--- rams/modules/cplink.py
+++ rams/modules/cplink.py
@@ -33,7 +33,41 @@ async def kangcopy(client: Client, message: Message):
             return await mmk.edit(message, f"**ERROR:** `{e}`")
         try:
             await asyncio.sleep(1.5)
-            tai = await mmk.edit("`Nyolong berhasil...`")
+            tai = await mmk.edit("`Berhasil Mencuri...`")
+            a = await client.send_message(bot, link)
+            await asyncio.sleep(2)
+            await a.delete()
+            await tai.delete()
+            async for c in client.get_chat_history(bot, limit=1):
+                await c.copy(message.chat.id)
+            await client.delete_message(bot, link)
+        except BaseException:
+            pass
+        try:
+            async for f in client.search_messages(message.chat.id, query="Trying to Download."):
+                await f.delete()
+            async for o in client.search_messages(message.chat.id, query="DOWNLOADING:"):
+                await o.delete()
+            async for g in client.search_messages(message.chat.id, query="Preparing to Upload!"):
+                await g.delete()
+        except BaseException:
+            pass
+
+@gez.on_message(filters.command("tt", [".", ",", "?", "!", "*", "$"]) & filters.me)
+async def kangtiktok(client: Client, message: Message):
+    mmk = await message.reply_text("`Processing . . .`")
+    link = get_arg(message)
+    bot = "downloader_tiktok_bot"
+    if link:
+        try:
+            await asyncio.sleep(1.5)
+            await client.join_chat("userbotch")
+            await client.join_chat("b4c0d")
+        except Exception as e:
+            return await mmk.edit(message, f"**ERROR:** `{e}`")
+        try:
+            await asyncio.sleep(1.5)
+            tai = await mmk.edit("`Berhasil Mendownload...`")
             a = await client.send_message(bot, link)
             await asyncio.sleep(2)
             await a.delete()
