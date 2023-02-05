de481c3411045408b83c1bacd1f374496dae2739
GeezRamPy <123511546+GeezRamPy@users.noreply.github.com>
2023-02-06 03:00:54 +0700

    Update data.py

--- rams/split/data.py
+++ rams/split/data.py
@@ -4,7 +4,7 @@ modules = CMD_HELP
 class Data:
 
     text_help_menu = (
-        f"**Menu Inline Ram-Pyro**\nModules : {len(modules)}\nPerintah : ? ! . * , $"
+        f"**Menu Inline Ram-Pyro**\nModules : </b> <code>{len(modules)} </code>\nPerintah : ? ! . * , $"
         .replace(",", "")
         .replace("[", "")
         .replace("]", "")
