53459c07ae0fa10e6144a3c1a6d80c5a9e69c84c
GeezRamPy <123511546+GeezRamPy@users.noreply.github.com>
2023-02-06 02:44:07 +0700

    Update data.py

--- rams/split/data.py
+++ rams/split/data.py
@@ -1,9 +1,10 @@
 from pyrogram.types import InlineKeyboardButton, WebAppInfo
-
+from rams import CMD_HELP
+modules = CMD_HELP
 class Data:
 
     text_help_menu = (
-        f"**《 Menu Inline RamPyro-Master 》**\n╰┈➤ perintah: ? ! . * , $"
+        f"**Menu Inline Ram-Pyro**\nModules : {modules}\nPerintah : ? ! . * , $"
         .replace(",", "")
         .replace("[", "")
         .replace("]", "")
