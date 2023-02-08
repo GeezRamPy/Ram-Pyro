94e6e80d423bd12d9abda1a72d1e0747675e5079
GeezRamPy <123511546+GeezRamPy@users.noreply.github.com>
2023-02-08 14:44:23 +0700

    Update catatan.py

--- rams/modules/catatan.py
+++ rams/modules/catatan.py
@@ -14,10 +14,10 @@ async def list_notes(client, message):
     user_id = message.from_user.id
     notes = get_notes(str(user_id))
     if not notes:
-        return await message.reply("**Tidak ada catatan.**")
-    msg = f"**Daftar catatan**\n"
+        return await message.reply("Tidak ada catatan.")
+    msg = f"Daftar catatan\n"
     for note in notes:
-        msg += f"* `{note.keyword}`\n"
+        msg += f"* {note.keyword}\n"
     await message.reply(msg)
 
 
@@ -27,9 +27,9 @@ async def remove_notes(client, message):
     user_id = message.from_user.id
     if rm_note(str(user_id), notename) is False:
         return await message.reply(
-            "**Tidak dapat menemukan catatan:** `{}`".format(notename)
+            "Tidak dapat menemukan catatan: {}".format(notename)
         )
-    return await message.reply("**Berhasil Menghapus Catatan:** `{}`".format(notename))
+    return await message.reply("Berhasil Menghapus Catatan: {}".format(notename))
 
 
 @Client.on_message(filters.command("save", cmd) & filters.me)
@@ -38,11 +38,11 @@ async def simpan_note(client, message):
     user_id = message.from_user.id
     msg = message.reply_to_message
     if not msg:
-        return await message.reply("__Tolong balas ke pesan__")
-    anu = await msg.forward(BOTLOG_CHATID)
+        return await message.reply("Tolong balas ke pesan")
+    anu = await msg.forward("me")
     msg_id = anu.id
     await client.send_message(
-        BOTLOG_CHATID,
+        "me",
         f"#NOTE\nKEYWORD: {keyword}"
         "\n\nPesan berikut disimpan sebagai data balasan catatan untuk obrolan, mohon JANGAN dihapus !!",
     )
@@ -58,5 +58,13 @@ async def panggil_notes(client, message):
     note = get_note(str(user_id), notename)
     if not note:
         return await message.reply("Tidak ada catatan seperti itu.")
-    msg_o = await client.get_messages(BOTLOG_CHATID, int(note.f_mesg_id))
-    await msg_o.copy(message.chat.id)
+    msg_o = await client.get_messages("me", int(note.f_mesg_id))
+    await msg_o.copy(message.chat.id, reply_to_message_id=message.id)
+
+
+modules_help["notes"] = {
+    "notes": "list all save notes",
+    "save": "save the replied message as a notes with name notename",
+    "clear": "clear note with this name",
+    "get": " get the note with thus notename",
+}
