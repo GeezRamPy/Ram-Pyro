5c5ce4f7926eba6ca734a25e224113c737f0cc9e
GeezRamPy <123511546+GeezRamPy@users.noreply.github.com>
2023-02-09 02:23:03 +0700

    Update notes_sql.py

--- rams/split/berak/SQL/notes_sql.py
+++ rams/split/berak/SQL/notes_sql.py
@@ -1,35 +1,33 @@
 from sqlalchemy import Column, Numeric, String, UnicodeText
 
-try:
-    from . import BASE, SESSION
-except ImportError:
+from . import BASE, SESSION
 
 
-class Notes(BASE):
-    __tablename__ = "notes"
+class Note(BASE):
+    tablename = "note"
     user_id = Column(String(14), primary_key=True)
     keyword = Column(UnicodeText, primary_key=True, nullable=False)
     f_mesg_id = Column(Numeric)
 
-    def __init__(self, user_id, keyword, f_mesg_id):
+    def init(self, user_id, keyword, f_mesg_id):
         self.user_id = str(user_id)
         self.keyword = keyword
         self.f_mesg_id = int(f_mesg_id)
 
 
-Notes.__table__.create(checkfirst=True)
+Note.table.create(checkfirst=True)
 
 
 def get_note(user_id, keyword):
     try:
-        return SESSION.query(Notes).get((str(user_id), keyword))
+        return SESSION.query(Note).get((str(user_id), keyword))
     finally:
         SESSION.close()
 
 
 def get_notes(user_id):
     try:
-        return SESSION.query(Notes).filter(Notes.user_id == str(user_id)).all()
+        return SESSION.query(Note).filter(Note.user_id == str(user_id)).all()
     finally:
         SESSION.close()
 
@@ -37,14 +35,14 @@ def get_notes(user_id):
 def add_note(user_id, keyword, f_mesg_id):
     to_check = get_note(user_id, keyword)
     if not to_check:
-        adder = Notes(str(user_id), keyword, f_mesg_id)
+        adder = Note(str(user_id), keyword, f_mesg_id)
         SESSION.add(adder)
         SESSION.commit()
         return True
-    rem = SESSION.query(Notes).get((str(user_id), keyword))
+    rem = SESSION.query(Note).get((str(user_id), keyword))
     SESSION.delete(rem)
     SESSION.commit()
-    adder = Notes(str(user_id), keyword, f_mesg_id)
+    adder = Note(str(user_id), keyword, f_mesg_id)
     SESSION.add(adder)
     SESSION.commit()
     return False
@@ -54,7 +52,7 @@ def rm_note(user_id, keyword):
     to_check = get_note(user_id, keyword)
     if not to_check:
         return False
-    rem = SESSION.query(Notes).get((str(user_id), keyword))
+    rem = SESSION.query(Note).get((str(user_id), keyword))
     SESSION.delete(rem)
     SESSION.commit()
     return True
