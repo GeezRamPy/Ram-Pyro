d743df970f4bd0c5770c715bd6ba498a1f66de1c
GeezRamPy <123511546+GeezRamPy@users.noreply.github.com>
2023-02-15 00:34:41 +0700

    Update start.py

--- rams/modules/bot/start.py
+++ rams/modules/bot/start.py
@@ -1,4 +1,4 @@
-# if you can read this, this meant you use code from Geez Ram Project
+# if you can read this, this meant you use code from Geez | Ram Project
 # this code is from somewhere else
 # please dont hestitate to steal it
 # because Geez and Ram doesn't care about credit
@@ -7,11 +7,42 @@
 #
 #
 # kopas repo dan hapus credit, ga akan jadikan lu seorang developer
-# ©2023 Geez & Ram Team
+# ©2023 Geez | Ram Team
+import random
 from rams import app
 from pyrogram import filters
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
+from config import ID_OWNER as owner 
 
+@app.on_callback_query()
+def pmowner(client, callback_query):
+    user_id = owner
+    message = "Halo Owner Ngentod!!!!"
+    client.send_message(user_id, message)
+    client.answer_callback_query(callback_query.id, text="Message sent")
+
+ramslogo = [
+    "https://telegra.ph/file/d03ce0fb84f81be3aeb09.png",
+    "https://telegra.ph/file/200355acbe58c46400f5b.png",
+    "https://telegra.ph/file/c78bb1efdeed38ee16eb2.png",
+    "https://telegra.ph/file/4143843c984a8ecdc813e.png"
+]
+
+alive_logo = random.choice(ramslogo)
 
 @app.on_message(filters.command("start") & filters.private)
-async def start(client, message):
-   await message.reply_text("Hey RamPyro-Bot Assistant here")
+async def start(app, message):
+    chat_id = message.chat.id
+    file_id = alive_logo
+    caption = "Halo anjing, Saya Asisstant Ram-Pyro\nJangan Cari Yang Spesial Dari saya."
+    reply_markup = InlineKeyboardMarkup([
+        [
+            InlineKeyboardButton("𝗦𝘂𝗽𝗽𝗼𝗿𝘁", url="https://t.me/GeezRam"),
+            InlineKeyboardButton("𝗥𝗲𝗽𝗼𝘀𝗶𝘁𝗼𝗿𝘆", url="https://github.com/GeezRampy/ram-pyro"),
+        ],
+    ])
+
+    await app.send_photo(chat_id, file_id, caption=caption, reply_markup=reply_markup)
